3f34e8725a52f109f972ece060e759b8c8005802
De Belser Arne <debelserarne@hotmail.com>
2020-09-03 13:20:54 +0200

    Added laravel auth api

--- routes/web.php
+++ routes/web.php
@@ -6,3 +6,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/data', function () {
     return LosData::get()->makeHidden(['id', 'created_at', 'updated_at']);
 });
+
+Auth::routes(['register' => false]);
