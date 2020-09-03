8a040e216e6bc10edd2c01e375669b96fa439c5b
De Belser Arne <debelserarne@hotmail.com>
2020-09-03 14:08:45 +0200

    wip

---
+++ app/Http/Controllers/DeviceController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Device;
+use Illuminate\Http\Request;
+
+class DeviceController extends Controller
+{
+    public function store(Request $request)
+    {
+        dd($request->all());
+        if (Device::where('token', $request->token)->first()) {
+            return response()->json("Device is already saved in the database", 200);
+        }
+
+        $device = Device::create([
+            'token' => $request->token,
+        ]);
+
+        return response()->json("A new device #{$device->id} with token: {$token} has been added", 200);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -5,25 +5,16 @@ use App\LosDataKbdb;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
-
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::middleware('auth:api')->get('/data-kbdb', function () {
+Route::middleware('auth:api')->get('/data/kbdb', function () {
     return LosDataKbdb::get()->makeHidden(['id', 'created_at', 'updated_at']);
 });
 
-Route::middleware('auth:api')->get('/data-duivenspel', function () {
+Route::middleware('auth:api')->get('/data/duivenspel', function () {
     return LosDataDuivenspel::get()->makeHidden(['id', 'created_at', 'updated_at']);
 });
+
+Route::middleware('auth:api')->get('/device/store', 'DeviceController@store')->name('device.store');
