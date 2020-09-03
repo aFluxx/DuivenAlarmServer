2c1d35410681aa7fbbcb26278eaaf3af31548169
De Belser Arne <debelserarne@hotmail.com>
2020-09-03 13:29:43 +0200

    Added api routes

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,11 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::middleware('auth:api')->get('/data-kbdb', function () {
+    return LosDataKbdb::get()->makeHidden(['id', 'created_at', 'updated_at']);
+});
+
+Route::middleware('auth:api')->get('/data-duivenspel', function () {
+    return LosDataDuivenspel::get()->makeHidden(['id', 'created_at', 'updated_at']);
+});
--- routes/web.php
+++ routes/web.php
@@ -6,12 +6,4 @@ use Illuminate\Support\Facades\Route;
 
 Auth::routes(['register' => false]);
 
-Route::get('/data-kbdb', function () {
-    return LosDataKbdb::get()->makeHidden(['id', 'created_at', 'updated_at']);
-});
-
-Route::get('/data-duivenspel', function () {
-    return LosDataDuivenspel::get()->makeHidden(['id', 'created_at', 'updated_at']);
-});
-
 Route::get('/kbdb/table/{table}', 'KBDBTableController@show')->name('table.show');
