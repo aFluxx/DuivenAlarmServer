fae4e384b0f666ff59a57e9ae076b53ded22c686
De Belser Arne <debelserarne@hotmail.com>
2020-09-15 13:56:26 +0200

    stop grinding kbdb for tables

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -25,7 +25,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         $schedule->command('kbdb:check-timestamp')->everyMinute();
-        $schedule->command('kbdb:grind')->everyTenMinutes();
+        // $schedule->command('kbdb:grind')->everyTenMinutes();
     }
 
     /**
